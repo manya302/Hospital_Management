6f05b06465f3c029003ef23d0f0fe0c72557629f
Manya Sinha <sinha.manya.30@gmail.com>
2026-02-21 12:48:00 +0530

    Creating tables

--- schema.sql
+++ schema.sql
@@ -1,10 +1,67 @@
-CREATE TABLE test_table (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100)
+CREATE TABLE patients (
+    patient_id VARCHAR(10) PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    gender VARCHAR(10),
+    date_of_birth DATE,
+    contact_number BIGINT,
+    address VARCHAR(255),
+    registration_date DATE,
+    insurance_provider VARCHAR(100),
+    insurance_number VARCHAR(50),
+    email VARCHAR(100)
 );
 
-insert into test_table values (1, 'Alice');
-select * from test_table;
 
-\d test_table;
+CREATE TABLE doctors (
+    doctor_id VARCHAR(10) PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    specialization VARCHAR(100),
+    phone_number BIGINT,
+    years_experience INT,
+    hospital_branch VARCHAR(100),
+    email VARCHAR(100)
+);
+
+
+CREATE TABLE appointments (
+    appointment_id VARCHAR(10) PRIMARY KEY,
+    patient_id VARCHAR(10),
+    doctor_id VARCHAR(10),
+    appointment_date DATE,
+    appointment_time TIME,
+    reason_for_visit VARCHAR(255),
+    status VARCHAR(50),
+
+    FOREIGN KEY (patient_id) REFERENCES patients(patient_id),
+    FOREIGN KEY (doctor_id) REFERENCES doctors(doctor_id)
+);
+
+
+CREATE TABLE treatments (
+    treatment_id VARCHAR(10) PRIMARY KEY,
+    appointment_id VARCHAR(10),
+    treatment_type VARCHAR(100),
+    description VARCHAR(255),
+    cost DECIMAL(10,2),
+    treatment_date DATE,
+
+    FOREIGN KEY (appointment_id) REFERENCES appointments(appointment_id)
+);
+
+
+CREATE TABLE billing (
+    bill_id VARCHAR(10) PRIMARY KEY,
+    patient_id VARCHAR(10),
+    treatment_id VARCHAR(10),
+    bill_date DATE,
+    amount DECIMAL(10,2),
+    payment_method VARCHAR(50),
+    payment_status VARCHAR(50),
+
+    FOREIGN KEY (patient_id) REFERENCES patients(patient_id),
+    FOREIGN KEY (treatment_id) REFERENCES treatments(treatment_id)
+);
+
 Select version();
